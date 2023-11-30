a689dba6f178ff8f6e1edd05a7ecb31a85b1b4f4
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-11-30 17:54:12 -0500

    changed seeeded data

--- seeds/blogData.js
+++ seeds/blogData.js
@@ -2,37 +2,37 @@ const blogData = [
      {
           title: "Blog Post 1",
           content: "This is the content for Blog Post 1.",
-          date_created: "2023-11-23T21:06:47.000Z",
+          // date_created: "2023-11-23T21:06:47.000Z",
           userId: 1,
      },
      {
           title: "Blog Post 2",
           content: "This is the content for Blog Post 2.",
-          date_created: "2023-11-23T21:06:47.000Z",
+          // date_created: "2023-11-23T21:06:47.000Z",
           userId: 2,
      },
      {
           title: "Blog Post 3",
           content: "This is the content for Blog Post 3.",
-          date_created: "2023-11-23T21:06:47.000Z",
+          // date_created: "2023-11-23T21:06:47.000Z",
           userId: 3,
      },
      {
           title: "Blog Post 4",
           content: "This is the content for Blog Post 4.",
-          date_created: "2023-11-23T21:06:47.000Z",
+          // date_created: "2023-11-23T21:06:47.000Z",
           userId: 4,
      },
      {
           title: "Blog Post 5",
           content: "This is the content for Blog Post 5.",
-          date_created: "2023-11-23T21:06:47.000Z",
+          // date_created: "2023-11-23T21:06:47.000Z",
           userId: 5,
      },
      {
           title: "Blog Post 6",
           content: "This is the content for Blog Post 6 by user 6.",
-          date_created: "2023-11-23T21:06:47.000Z",
+          // date_created: "2023-11-23T21:06:47.000Z",
           userId: 6,
      },
 ];
--- seeds/commentData.js
+++ seeds/commentData.js
@@ -1,70 +1,70 @@
 const commentData = [
      {
-          comment_text: "This is a comment for Blog Post 1 by User 1.",
+          comment_text: "This is a comment for Blog Post 1.",
           date_created: "2023-11-23T21:06:47.000Z",
           userId: 1,
           blogId: 1,
      },
      {
-          comment_text: "This is a comment for Blog Post 1 by User 2.",
+          comment_text: "This is a comment for Blog Post 1.",
           date_created: "2023-11-24T10:15:30.000Z",
           userId: 2,
           blogId: 1,
      },
      {
-          comment_text: "This is a comment for Blog Post 2 by User 3.",
+          comment_text: "This is a comment for Blog Post 2.",
           date_created: "2023-11-25T14:20:15.000Z",
           userId: 3,
           blogId: 2,
      },
      {
-          comment_text: "This is a comment for Blog Post 2 by User 4.",
+          comment_text: "This is a comment for Blog Post 2.",
           date_created: "2023-11-26T18:25:45.000Z",
           userId: 4,
           blogId: 2,
      },
      {
-          comment_text: "This is a comment for Blog Post 3 by User 5.",
+          comment_text: "This is a comment for Blog Post 3.",
           date_created: "2023-11-27T22:30:30.000Z",
           userId: 5,
           blogId: 3,
      },
      {
-          comment_text: "This is a comment for Blog Post 3 by User 1.",
+          comment_text: "This is a comment for Blog Post 3.",
           date_created: "2023-11-28T02:35:15.000Z",
           userId: 1,
           blogId: 3,
      },
      {
-          comment_text: "This is a comment for Blog Post 4 by User 2.",
+          comment_text: "This is a comment for Blog Post 4.",
           date_created: "2023-11-29T06:40:45.000Z",
           userId: 2,
           blogId: 4,
      },
      {
-          comment_text: "This is a comment for Blog Post 4 by User 3.",
+          comment_text: "This is a comment for Blog Post 4.",
           date_created: "2023-11-30T10:45:30.000Z",
           userId: 3,
           blogId: 4,
      },
      {
-          comment_text: "This is a comment for Blog Post 5 by User 4.",
+          comment_text: "This is a comment for Blog Post 5.",
           date_created: "2023-12-01T14:50:15.000Z",
           userId: 4,
           blogId: 5,
      },
      {
-          comment_text: "This is a comment for Blog Post 5 by User 5.",
+          comment_text: "This is a comment for Blog Post 5.",
           date_created: "2023-12-02T18:55:45.000Z",
           userId: 5,
           blogId: 5,
      },
      {
-          comment_text: "This is a comment for Blog Post 6 by User u.",
+          comment_text: "This is a comment for Blog Post 6.",
           date_created: "2023-12-03T23:00:30.000Z",
           userId: 6,
           blogId: 6,
-     }
+     },
 ];
 
 export default commentData;
